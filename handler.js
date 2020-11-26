b02cc9eb29079e7868338cbf44919ddd40739619
Koh Choong Yong <choongyong.koh@gmail.com>
2020-11-26 16:30:58 +0800

    changed: use async signature

--- handler.js
+++ handler.js
@@ -1,16 +1,18 @@
 'use strict';
 
-module.exports.helloWorld = (event, context, callback) => {
+module.exports.helloWorld = async (event, context) => {
+  const body = JSON.stringify({
+    message: 'Go Serverless v1.0! My function executed successfully!',
+    input: event,
+  });
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: body,
   };
 
-  callback(null, response);
+  console.log(body);
+  return response;
 };
