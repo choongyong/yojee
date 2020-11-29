5c36ddb89623417e708bac60df57b5716726d9fc
Koh Choong Yong <choongyong.koh@gmail.com>
2020-11-29 10:54:56 +0800

    changed: correctly convert weight from grams to kilograms

--- item.js
+++ item.js
@@ -1,7 +1,7 @@
 const itemFactory = (item) => {
   return {
     "width": 1,
-    "weight": item.grams,
+    "weight": item.grams / 1000,
     "volumetric_weight": 0.0002,
     "volume": 1,
     "service_type_id": 1,
