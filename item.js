e025c4e17d4fa931de8d1351d89f4e27d50e483d
Koh Choong Yong <choongyong.koh@gmail.com>
2020-11-29 18:40:04 +0800

    changed: calculate price and weight based on quantity

--- item.js
+++ item.js
@@ -1,12 +1,12 @@
 const itemFactory = (item) => {
   return {
     "width": 1,
-    "weight": item.grams / 1000,
-    "volumetric_weight": 0.0002,
+    "weight": item.quantity * (item.grams / 1000),
+    "volumetric_weight": 1,
     "volume": 1,
     "service_type_id": 1,
     "service_type": "same_day",
-    "price_amount": item.price,
+    "price_amount": (item.quantity * item.price).toFixed(2),
     "payload_type": "parcel",
     "length": 1,
     "height": 1
